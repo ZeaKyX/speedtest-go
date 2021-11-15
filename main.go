6c1f1552c213368f6e4a1bfe1a9a374310fbb668
ZeaKyX <43315078+ZeaKyX@users.noreply.github.com>
2021-11-15 19:05:15 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
-	"flag"
 	_ "time/tzdata"
+
+	"github.com/spf13/pflag"
+	"github.com/spf13/viper"
 
 	"github.com/librespeed/speedtest/config"
 	"github.com/librespeed/speedtest/database"
@@ -14,12 +16,29 @@ import (
 )
 
 var (
-	optConfig = flag.String("c", "", "config file to be used, defaults to settings.toml in the same directory")
+	optConfig = pflag.StringP("configfilepath", "c", "", "config file to be used, defaults to settings.toml in the same directory")
 )
 
+func init() {
+	pflag.StringP("listen_port", "p", "", "backend listen port, default is 8989")
+	pflag.StringP("proxyprotocol_port", "", "", "proxy protocol port, use 0 to disable")
+	pflag.StringP("download_chunks", "d", "", "")
+	pflag.StringP("distance_unit", "", "", "")
+	pflag.StringP("enable_cors", "e", "", "")
+	pflag.StringP("statistics_password", "s", "", "")
+	pflag.StringP("redact_ip_addresses", "r", "", "")
+	pflag.StringP("database_type", "", "", "")
+	pflag.StringP("database_hostname", "", "", "")
+	pflag.StringP("database_name", "", "", "")
+	pflag.StringP("database_username", "", "", "")
+
+	viper.BindPFlags(pflag.CommandLine)
+}
+
 func main() {
-	flag.Parse()
+	pflag.Parse()
 	conf := config.Load(*optConfig)
+	viper.Unmarshal(&conf)
 	web.SetServerLocation(&conf)
 	results.Initialize(&conf)
 	database.SetDBInfo(&conf)
